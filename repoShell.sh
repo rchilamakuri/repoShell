8ae0a68240175b201ea68ccf83df6f7df75436bb
Ubuntu <ubuntu@ip-172-31-41-177.us-east-2.compute.internal>
2019-12-31 19:17:17 +0000

    updating the git repo with the branch and the files

--- repoShell.sh
+++ repoShell.sh
@@ -10,18 +10,45 @@
 #echo The folder is deleted
 
 
-echo the commit check is done correctly
+#echo the commit check is done correctly
 
-echo The second command which is done for checking the line in the repository
+#echo The second command which is done for checking the line in the repository
 
 #echo "checking weather the text is getting spoied in to the file or not" >> newFile.sh
+
+#git status
+
+#git add .
+
+#git commit -m "this is the commit done from the shell script"
+
+#git push origin master
+
+
+git checkout -b feature/firstBranchInRepo
+
+functionOne(){
+
+echo "this is the line which needs to be entered in the newfile from master brnach" >> newFile.sh
+
+git push origin master
+
+}
+
+funtionTwo(){
+
+echo "this is the line which needs to be entered in the newfile from feature branch" >> newFile2.sh
+
+git push origin feature/firstBranchInRepo
+
+}
+
 
 git status
 
 git add .
 
-git commit -m "this is the commit done from the shell script"
+git commit -m "updating the git repo with the branch and the files"
 
-git push origin master
-
-
+functionOne()
+functionTwo()
